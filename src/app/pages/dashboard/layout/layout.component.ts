2c049c8bf4bda81aff7a3fcadd3266adff669516
Ronald <torresfloronald@gmail.com>
2024-06-14 15:40:58 -0500

    Ajuste en dashboard

--- src/app/pages/dashboard/layout/layout.component.ts
+++ src/app/pages/dashboard/layout/layout.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TypologyInventory } from 'src/app/models/typologyinventory';
+import { Workplan } from 'src/app/models/workplan';
+import { DashboardService } from 'src/app/services/dashboard.service';
+import { WorkplanService } from 'src/app/services/workplan.service';
+import { IMAGE_SIZE } from 'src/app/utils/constants';
+import { MESSAGE } from 'src/labels/labels';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
+  protected readonly Math = Math;
+  protected readonly MESSAGE = MESSAGE;
+  protected readonly IMAGE_SIZE = IMAGE_SIZE;
 
+  workplans: Workplan[];
+  inventory: TypologyInventory[];
+  loading: boolean = false;
+
+  constructor(private workplanService: WorkplanService, private dashboardService: DashboardService) {}
+
+  ngOnInit() {
+    this.getWorkplans();
+  }
+
+  getWorkplans() {
+    this.loading = true;
+    this.workplanService.getWorkplans().subscribe({
+      next: (data) => {
+        this.workplans = data;
+        this.loading = false;
+        this.getInventarioTipologia();
+      },
+    });
+  }
+
+  getInventarioTipologia() {
+    this.loading = true;
+    this.dashboardService.getInventory().subscribe({
+      next: (data) => {
+        this.inventory = data;
+        this.loading = false;
+      },
+    });
+  }
 }
--- src/app/pages/dashboard/layout/layout.module.ts
+++ src/app/pages/dashboard/layout/layout.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
-import { StructureResumeComponent } from './structure-resume/structure-resume.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ChartsComponent } from './charts/charts.component';
-import {RippleModule} from "primeng/ripple";
-import { WorkplanResumeComponent } from './workplan-resume/workplan-resume.component';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent,StructureResumeComponent, ChartsComponent, WorkplanResumeComponent
+    LayoutComponent, ChartsComponent
   ],
     imports: [
         LayoutRoutingModule, SharedModule
