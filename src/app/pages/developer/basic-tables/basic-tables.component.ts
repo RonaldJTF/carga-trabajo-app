878cfe19930585f8f79c1265f78a40aad4c4fe4e
Ronald <torresfloronald@gmail.com>
2024-09-26 17:31:50 -0500

    Ajuste de labels en tablas básicas

--- src/app/pages/developer/basic-tables/basic-tables.component.ts
+++ src/app/pages/developer/basic-tables/basic-tables.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {MegaMenuItem, Message} from "primeng/api";
 import {MESSAGE} from "@labels/labels";
-import {Router} from "@angular/router";
 import {Location} from "@angular/common";
 import {MenuService} from "@services";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-basic-tables',
@@ -58,74 +58,74 @@ export class BasicTablesComponent implements OnInit {
   initializeOptions() {
     this.menuItems = [
       {
-        title: 'Acción',
+        title: 'Acciones',
         icon: 'pi pi-verified',
         url: 'action',
-        label: 'Configure y gestione las acciones disponibles en el sistema.',
+        label: 'Gestión de las acciones que se pueden realizar en el sistema',
         iconStyle: 'primary',
         state: {
-          message: 'Esto puede afectar la funcionalidad y la experiencia de usuario. Por favor, revise cuidadosamente los cambios y asegúrese de que no esté eliminando funcionalidades esenciales.',
+          message: 'Esto puede afectar el acceso a las funcionalidades ya relacionadas y por lo tanto, la experiencia del usuario. Por favor, revise cuidadosamente los cambios y asegúrese de que no eliminar acceso a funcionalidades esenciales.',
           severity: 'warn',
           show: true
         }
       },
       {
-        title: 'Ftp',
+        title: 'Servidores FTPs',
         icon: 'pi pi-folder',
         url: 'ftp',
-        label: 'Configure y administre los servidores FTP.',
+        label: 'Gestión de los servidores FTPs que alojan archivos soportes',
         iconStyle: 'primary',
         state: {
-          message: 'Realizar modificaciones en la configuracion del los servidores FTP puede causar interrupciones en el servicio. Impactando en la conectividad y el acceso a los archivos.',
+          message: 'Realizar modificaciones en la configuración de los servidores FTPs puede causar inconvenientes en el acceso y almacenamiento de los archivos soportes.',
           severity: 'warn',
           show: true
         }
       },
       {
-        title: 'Género',
+        title: 'Géneros',
         icon: 'pi pi-circle',
         url: 'gender',
-        label: 'Gestiona las categorías de género disponibles para los perfiles de usuario.',
+        label: 'Gestión de las categorías de los géneros disponibles para los perfiles de usuario',
         iconStyle: 'primary',
         state: {}
       },
       {
-        title: 'Nivel ocupacional',
+        title: 'Niveles ocupacionales',
         icon: 'pi pi-bookmark',
         url: 'level',
-        label: 'Define y administra los niveles ocupacionales dentro de la organización.',
+        label: 'Gestón de los niveles ocupacionales dentro de la organización',
         iconStyle: 'primary',
         state: {}
       },
       {
-        title: 'Rol',
+        title: 'Roles de acceso',
         icon: 'pi pi-users',
         url: 'rol',
-        label: 'Administra y define los roles de usuario en el sistema.',
+        label: 'Gestión de los roles de usuario en el sistema',
         iconStyle: 'primary',
         state: {
-          message: 'Hacer cambios en los roles puede impactar la seguridad y el control de acceso al aplicativo. Por favor, revise cuidadosamente los cambios.',
+          message: 'Realizar cambios en los roles puede impactar en la seguridad y en el control de acceso del aplicativo. Por favor, revise cuidadosamente los cambios.',
           severity: 'warn',
           show: true
         }
       },
       {
-        title: 'Tipologia',
+        title: 'Tipologías',
         icon: 'pi pi-sitemap',
         url: 'typology',
-        label: 'Administra las diferentes tipologías disponibles para clasificación de las estructuras.',
+        label: 'Gestión de las tipologías para la categorización de las estructuras',
         iconStyle: 'primary',
         state: {
-          message: 'Realizar modificaciones en las tipologias puede impactar la integridad y la coherencia de los datos. Por favor, revise cuidadosamente los cambios y asegúrese de que no esté causando inconsistencias.',
+          message: 'Realizar modificaciones en las tipologías puede impactar en la integridad y la coherencia de los datos. Por favor, revise cuidadosamente los cambios y asegúrese de que no esté causando inconsistencias.',
           severity: 'warn',
           show: true
         }
       },
       {
-        title: 'Documento',
+        title: 'Documentos de identidad',
         icon: 'pi pi-id-card',
         url: 'document-type',
-        label: 'Configura y administra el tipo de documento.',
+        label: 'Gestión de los tipos de documentos de identidad',
         iconStyle: 'primary',
         state: {}
       }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,4 @@ export const environment = {
 
   URLPAGE: "https://proyecto.unipamplona.edu.co/carga-trabajo-app/",
   API_REG_EXP: /^https:\/\/proyecto.unipamplona.edu.co\/carga-trabajo-services\/api/
-
-
-
-  /*URLAPI: "http://172.26.123.123:9500/carga-trabajo-services/api",
-
-  URLPAGE: "http://172.26.123.123:9500/carga-trabajo-app/",
-  API_REG_EXP: /^http:\/\/172.26.123.123:9500\/carga-trabajo-services\/api/*/
 };
